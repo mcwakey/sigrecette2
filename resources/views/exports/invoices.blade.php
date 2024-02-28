12eb8b74c6ee55efdf0dabe9b3d2e8f22d91312f
LanzaDelRelampago <novenosexta77@gmail.com>
2024-02-28 12:16:45 +0000

    Update invoices.blade.php

--- resources/views/exports/invoices.blade.php
+++ resources/views/exports/invoices.blade.php
@@ -120,7 +120,8 @@
 
     <div class="container avis-container">
         <div class="count_page">
-            <h6>page <span>{{$index+1 .'/'.sizeof($data[12]) }} </span></h6>
+
+            <h6>@if(sizeof($data[12]) >1)  pages   @endif <span>{{$index+1 .'/'.sizeof($data[12]) }} </span></h6>
         </div>
         <table>
             <tr class="text-start">
@@ -165,7 +166,7 @@
             <p>Madame, Mademoiselle, Monsieur,</p>
             <p>Vous êtes priés de bien vouloir payer à la régie des recettes de la mairie de {{$data[6]}}
                 le montant ci-dessous :</p>
-            <p>N° d’ordre de recette :{{$data[1]}}<span class="write">/{{$data[0]}}</p>
+            <p>N° d’ordre de recette :<span class="write">{{$data[1]}}/</span>{{$data[0]}}</p>
 
             <p>Libellé de la recette :<span class="write"> {{$item[0]}}</span></p>
             <p>Imputation budgétaire :<span class="write"> {{$item[1]}}</span></p>
