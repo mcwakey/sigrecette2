5c0ba725378d9881ca7448e1d1513f6a8172d61e
LanzaDelRelampago <novenosexta77@gmail.com>
2024-03-20 12:30:39 +0000

    delete: year exercie on addInvoice

--- resources/views/livewire/invoice/auto-invoice-modal.blade.php
+++ resources/views/livewire/invoice/auto-invoice-modal.blade.php
@@ -113,15 +113,7 @@
                             </div>
 
                         </div>
-                        <div class="row">
-                            <div class="notice d-flex bg-light-primary rounded border-primary border border-dashed mb-1 p-2">
 
-                            <div class="col-md-2">
-                                <input type="text" class="required form-control form-control-flush text-end" placeholder="{{ __('4 exercices') }}" readonly />
-                            </div>
-
-                            </div>
-                        </div>
 
                         <div class="separator separator-dashed my-4"></div>
 
