7678b62e4686667df7e2072d118368a26b67f20a
LanzaDelRelampago <novenosexta77@gmail.com>
2024-03-21 11:35:24 +0000

    Update _menu.blade.php

--- resources/views/layout/partials/sidebar-layout/header/_menu/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/header/_menu/_menu.blade.php
@@ -9,7 +9,7 @@
 		<div data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="bottom-start" class="menu-item here show menu-here-bg menu-lg-down-accordion me-0 me-lg-2">
 			<!--begin:Menu link-->
 			<span class="menu-link">
-				<span class="menu-title">Annee d'exerce:{{" ".$year->name}}</span>
+				<span class="menu-title">Annee d'exercice:{{" ".$year->name}}</span>
 				<span class="menu-arrow d-lg-none"></span>
 			</span>
 			<!--end:Menu link-->
@@ -18,7 +18,7 @@
 			</div>
 			<!--end:Menu sub-->
 		</div>
-        
+
         <!--end::Menu item-->
 
         <!--begin::Menu item-->
