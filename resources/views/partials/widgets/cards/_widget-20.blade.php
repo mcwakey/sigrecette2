9ef18c37ee9f1b5a17fef4651da1aaa7e6f77a40
LanzaDelRelampago <novenosexta77@gmail.com>
2024-03-30 18:34:49 +0000

    Update _widget-20.blade.php

--- resources/views/partials/widgets/cards/_widget-20.blade.php
+++ resources/views/partials/widgets/cards/_widget-20.blade.php
@@ -17,23 +17,14 @@
 
 			<!--end::Subtitle-->
 		</div>
-        <div class="card-title d-flex flex-column">
-            <!--begin::Amount-->
-            <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2">{{$count_invoices["Total"]}}</span>
-            <!--end::Amount-->
-            <!--begin::Subtitle-->
-            <span class="text-white opacity-75 pt-1 fw-semibold fs-6">Total Invoices created</span>
-
-            <!--end::Subtitle-->
-        </div>
 		<!--end::Title-->
 	</div>
 	<!--end::Header-->
-	<!--begin::Card body-->
-	<div class="card-body d-flex align-items-end pt-0">
-		<!--begin::Progress-->
+	<!--
+<div class="card-body d-flex align-items-end pt-0">
 		<div class="d-flex align-items-center flex-column mt-3 w-100">
             <div class="d-flex justify-content-between fw-bold fs-6 text-white opacity-75 w-100 mt-auto mb-2">
+				
                 <span>{{ $count_invoices["APROVED"]." " }}APROVED</span>
                 <span>{{ $count_invoices["REJECTED"]." " }}Rejected</span>
                 <span> {{$count_invoices["REDUCED"]." "}} Reduced</span>
@@ -47,8 +38,12 @@
 				<div class="bg-white rounded h-8px" role="progressbar" style="width: {{round( ($count_invoices["Pending"] *100)/$count_invoices["NOEXPIRED"]) }}}}%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
 			</div>
 		</div>
-		<!--end::Progress-->
+	
 	</div>
-	<!--end::Card body-->
+
+
+-->
+	
+	
 </div>
 <!--end::Card widget 20-->
