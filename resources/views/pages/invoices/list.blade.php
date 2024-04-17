8b85278af8cec8dc2e33d7b1de59b73e06366f7b
LanzaDelRelampago <novenosexta77@gmail.com>
2024-04-17 21:39:16 +0000

    Update list.blade.php

--- resources/views/pages/invoices/list.blade.php
+++ resources/views/pages/invoices/list.blade.php
@@ -135,10 +135,7 @@
                             <label class="fs-6 form-label fw-bold text-dark">{{ __('invoice no') }}</label>
                             <input type="text" class="form-control" name="tags" id="mySearchTwo"/>
                         </div>
-                        <div class="col-xxl-2">
-                            <label class="fs-6 form-label fw-bold text-dark">{{ __('invoice no')."à" }}</label>
-                            <input type="text" class="form-control" name="tags" id="mySearchTwo0"/>
-                        </div>
+                       
                         <!--end::Col-->
                         <!--begin::Col-->
                         <div class="col-xxl-2">
