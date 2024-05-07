d7b25ca7dba737089fd29c8184725397e9a3be30
LanzaDelRelampago <novenosexta77@gmail.com>
2024-05-07 21:43:34 +0000

    Update AuthController.php

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends Controller
             'access_token' => $token,
             'user_id' => $user->id,
             'name' => $user->name,
+            'email', $user->email,
             'role' => $user->getRoleNames()->first(),
             'zone' => $user->zone->name,
         ], 200);
