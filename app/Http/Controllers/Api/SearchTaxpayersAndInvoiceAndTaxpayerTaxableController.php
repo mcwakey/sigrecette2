231d93c95c39436404fa4bbdeaae6e60fb7f3438
LanzaDelRelampago <novenosexta77@gmail.com>
2024-05-12 07:01:19 +0000

    add tapayers to synchro out

--- app/Http/Controllers/Api/SearchTaxpayersAndInvoiceAndTaxpayerTaxableController.php
+++ app/Http/Controllers/Api/SearchTaxpayersAndInvoiceAndTaxpayerTaxableController.php
@@ -2,35 +2,40 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\SearchTaxpayerResource;
 use App\Http\Resources\SearchTaxpayerTaxableResource;
 use App\Http\Resources\SearchInvoiceResource;
 use App\Models\Invoice;
+use App\Models\Taxpayer;
 use App\Models\TaxpayerTaxable;
 use App\Models\Zone;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class SearchInvoiceAndTaxpayerTaxableController extends Controller
+class SearchTaxpayersAndInvoiceAndTaxpayerTaxableController extends Controller
 {
     public function search(Request $request)
     {
         $zoneName = $request->input('zone', null);
 
-        $queryTaxpayer = TaxpayerTaxable::query();
+        $queryTaxpayerTaxable = TaxpayerTaxable::query();
         $queryInvoice = Invoice::query();
+        $queryTaxpayer = Taxpayer::query();
 
         if ($zoneName) {
             $zone = Zone::where('name', 'like', '%' . $zoneName . '%')->first();
             if ($zone) {
-                $queryTaxpayer->whereHas('taxpayer', function ($query) use ($zone) {
+                $queryTaxpayerTaxable->whereHas('taxpayer', function ($query) use ($zone) {
                     $query->where('zone_id', $zone->id);
                 });
 
                 $queryInvoice->whereHas('taxpayer', function ($query) use ($zone) {
                     $query->where('zone_id', $zone->id);
                 });
+                $queryTaxpayer->where('zone_id', $zone->id);
             } else {
                 return  [
+                    'taxpayers'=> SearchTaxpayerResource::collection(collect([])),
                     'taxpayer_taxables' =>   SearchTaxpayerTaxableResource::collection(collect([])),
                     'invoices' => SearchInvoiceResource::collection(collect([])),
                 ];
@@ -38,7 +43,8 @@ class SearchInvoiceAndTaxpayerTaxableController extends Controller
         }
 
         return [
-            'taxpayer_taxables' => SearchTaxpayerTaxableResource::collection($queryTaxpayer->get()),
+            'taxpayers'=> SearchTaxpayerResource::collection( $queryTaxpayer->get()),
+            'taxpayer_taxables' => SearchTaxpayerTaxableResource::collection($queryTaxpayerTaxable->get()),
             'invoices' => SearchInvoiceResource::collection($queryInvoice->get()),
         ];
     }
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Actions\SampleRoleApi;
 use App\Actions\SampleUserApi;
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\CheckIpAddress;
-use App\Http\Controllers\Api\SearchInvoiceAndTaxpayerTaxableController;
+use App\Http\Controllers\Api\SearchTaxpayersAndInvoiceAndTaxpayerTaxableController;
 use App\Http\Controllers\Api\SearchInvoiceController;
 use App\Http\Controllers\Api\SearchTaxLabelController;
 use App\Http\Controllers\Api\SearchTaxpayerController;
@@ -35,7 +35,7 @@ Route::post('/v1/check', [CheckIpAddress::class, 'check']);
 Route::Post('/v1/search/taxpayers', [SearchTaxpayerController::class, 'search']);
 Route::Post('/v1/search/taxpayerstaxables', [SearchTaxpayerTaxableController::class, 'search']);
 Route::Post('/v1/search/invoices', [SearchInvoiceController::class, 'search']);
-Route::Post('/v1/synchronisation/out', [ SearchInvoiceAndTaxpayerTaxableController::class, 'search']);
+Route::Post('/v1/synchronisation/out', [ SearchTaxpayersAndInvoiceAndTaxpayerTaxableController::class, 'search']);
 
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
