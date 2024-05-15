9761d75aa22085bfd916dc747b691f7f7a1244f3
mcwakey <mcwakey@yahoo.fr>
2024-05-15 07:01:54 +0000

    FIX:collector deposit printout

--- app/Models/StockTransfer.php
+++ app/Models/StockTransfer.php
@@ -42,25 +42,26 @@ class StockTransfer extends Model
     }
     public static function buildAndGetStockRequestWithQuery($id)
     {
-        $builder= StockRequest::join('taxables', 'stock_transfers.taxable_id', '=', 'taxables.id')
+        $builder= StockTransfer::join('taxables', 'stock_transfers.taxable_id', '=', 'taxables.id')
             ->join('users', 'stock_transfers.to_user_id', '=', 'users.id')
             ->select('stock_transfers.trans_id',
-                DB::raw('SUM(CASE WHEN trans_type = "RECU" THEN qty END) AS rc_qty'),
-                DB::raw('SUM(CASE WHEN trans_type = "VENDU" THEN qty END) AS vv_qty'),
-                DB::raw('MAX(CASE WHEN trans_type = "RENDU" THEN qty END) AS rd_qty'),
-                DB::raw('MAX(stock_transfers.id) AS id'),
-                DB::raw('MAX(stock_transfers.trans_no) AS trans_no'),
+                DB::raw('CASE WHEN trans_type = "RECU" THEN qty END AS rc_qty'),
+                DB::raw('CASE WHEN trans_type = "VENDU" THEN qty END AS vv_qty'),
+                DB::raw('CASE WHEN trans_type = "RENDU" THEN qty END AS rd_qty'),
+                DB::raw('stock_transfers.id AS id'),
+                DB::raw('stock_transfers.trans_no AS trans_no'),
                 //DB::raw('MAX(stock_transfers.trans_desc) AS trans_desc'),
-                DB::raw('MAX(stock_transfers.start_no) AS start_no'),
-                DB::raw('MAX(stock_transfers.end_no) AS end_no'),
-                DB::raw('MAX(stock_transfers.last_no) AS last_no'),
-                DB::raw('MIN(stock_transfers.trans_type) AS trans_type'),
-                DB::raw('MIN(stock_transfers.type) AS type'),
-                DB::raw('MAX(stock_transfers.to_user_id) AS to_user_id'),
-                DB::raw('MAX(stock_transfers.created_at) AS created_at'),
-                DB::raw('MAX(stock_transfers.taxable_id) AS taxable_id'))
+                DB::raw('stock_transfers.start_no AS start_no'),
+                DB::raw('stock_transfers.end_no AS end_no'),
+                DB::raw('stock_transfers.last_no AS last_no'),
+                DB::raw('stock_transfers.trans_type AS trans_type'),
+                DB::raw('stock_transfers.type AS type'),
+                DB::raw('stock_transfers.to_user_id AS to_user_id'),
+                DB::raw('stock_transfers.created_at AS created_at'),
+                DB::raw('stock_transfers.taxable_id AS taxable_id'))
             ->where('stock_transfers.to_user_id', $id)
-            ->groupBy('stock_transfers.trans_id')
+            ->where('stock_transfers.type', "ACTIVE")
+            // ->groupBy('stock_transfers.trans_id')
             ->orderBy('trans_id', 'desc');
         return $builder->get();
     }
