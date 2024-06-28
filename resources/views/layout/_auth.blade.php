c2fa7cb1e3507b18a32be6cec91b069453fa992f
dewunis <afanwouboh@gmail.com>
2024-06-28 13:02:58 +0000

    refactor: update logo height

--- resources/views/layout/_auth.blade.php
+++ resources/views/layout/_auth.blade.php
@@ -13,7 +13,7 @@
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
                     <!--begin::Logo-->
                     <a href="{{ route('dashboard') }}" class="mb-12">
-                        <img alt="Logo" src="{{ image('logos/custom-3.png') }}" class="h-100px" />
+                        <img alt="Logo" src="{{ image('logos/custom-3.png') }}" class="h-180px" />
                     </a>
 
                     <!--begin::Title-->
