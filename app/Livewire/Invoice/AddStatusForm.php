88973629a92639f7e4674ccc711eda4bd42f0aab
LanzaDelRelampago <59964509+novenopatch@users.noreply.github.com>
2024-07-03 16:50:41 +0000

    fix

--- app/Livewire/Invoice/AddStatusForm.php
+++ app/Livewire/Invoice/AddStatusForm.php
@@ -105,6 +105,7 @@ class AddStatusForm extends Component
                         'invoice_type' => $description_str,
                         'status' => $description_str,
                         'payment_type' => $description_str,
+                          'code'=>null
 
                     ];
                     $payments = Invoice::getCode($invoice->invoice_no, $invoice->reduce_amount, $paymentData);
