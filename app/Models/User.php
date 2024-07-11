a0b7f97fa90e8b31599b8a100bba54c9838ab26e
dewunis <afanwouboh@gmail.com>
2024-07-11 08:27:57 +0000

    fix: password hash

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,6 +53,7 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
         'last_login_at' => 'datetime',
+        'password' => 'hashed',
     ];
 
     public function getProfilePhotoUrlAttribute()
