0ebbfea5bfac781a5e978142424c65359f49b3e2
dewunis <afanwouboh@gmail.com>
2024-07-22 17:59:14 -0700

    chore: set master scrpits to async and defer

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -22,14 +22,14 @@
     <link rel="stylesheet" href="/assets/js/leaftlet/MarkerCluster.Default.css">
     <script async defer src="/assets/js/leaftlet/markercluster.js"></script>
 
-    <!-- amr5 graphique -->
-    <script src="assets/js/am5/percent.js"></script>
-    <script src="assets/js/am5/index.js"></script>
-    <script src="assets/js/am5/xy.js"></script>
-    <script src="assets/js/am5/animated.js"></script>
-    <script src="assets/js/am5/de_de.js"></script>
-    <script src="assets/js/am5/germany_low.js"></script>
-    <script src="assets/js/am5/notosans_sc.js"></script>
+    <!-- amr5 graphiques -->
+    <script async defer src="assets/js/am5/percent.js"></script>
+    <script async defer src="assets/js/am5/index.js"></script>
+    <script async defer src="assets/js/am5/xy.js"></script>
+    <script async defer src="assets/js/am5/animated.js"></script>
+    <script async defer src="assets/js/am5/de_de.js"></script>
+    <script async defer src="assets/js/am5/germany_low.js"></script>
+    <script async defer src="assets/js/am5/notosans_sc.js"></script>
 
     <link rel="icon" type="image/png" href="assets/media/logos/logo.png" sizes="16x16 32x32" />
     <link rel="stylesheet" href="/assets/css/fonts.css">
