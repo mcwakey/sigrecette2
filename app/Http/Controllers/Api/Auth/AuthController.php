ef091e47ae5d6a5c42cc4b419115230f2c86bdca
LanzaDelRelampago <novenosexta77@gmail.com>
2024-11-14 07:16:38 +0000

    feat: send users roles on api

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -31,6 +31,7 @@ class AuthController extends Controller
             'name' => $user->name,
             'email' => $user->email,
             'role' => $user->getRoleNames()->first(),
+            'permissions' => $user->getAllPermissions()->pluck('name'),
             'zone' => $user->zone->name,
         ], 200);
     }
