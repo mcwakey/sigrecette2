3a89cb7f75bdf23e782d8d4f506aeb8e800503ee
LanzaDelRelampago <novenosexta77@gmail.com>
2024-12-27 10:25:50 +0000

    feat: filter app by two perido

--- resources/views/layout/partials/sidebar-layout/header/_menu/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/header/_menu/_menu.blade.php
@@ -250,13 +250,10 @@
     <!--begin::Menu-->
     <div class="menu menu-rounded menu-column menu-lg-row my-5 my-lg-0 align-items-stretch fw-semibold px-2 px-lg-0"
         id="kt_app_header_menu" data-kt-menu="true" style="position: relative">
-        <!--begin:Menu item-->
         <div id="stats-date-btn" data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
             data-kt-menu-placement="bottom-start"
             class="menu-item here show menu-here-bg menu-lg-down-accordion me-0 me-lg-2">
-            <!--begin:Menu link-->
             <span class="menu-link">
-                {{--  --}}
                 <span class="menu-title">
                     <span>
                         @if ($commune != null)
@@ -278,7 +275,7 @@
 
 
         </div>
-
+        <form method="GET" action="{{ request()->url() }}">
         <div class="shadow-sm bg-white" id="stats-date"
             style="position:fixed;max-width:620px;display:none;align-items:center;height:100px;padding:16px 20px;gap:16px;top:60px;border-radius:6px;">
 
@@ -291,7 +288,7 @@
                         <i class="ki-duotone ki-calendar fs-2"><span class="path1"></span><span
                                 class="path2"></span></i>
                     </span>
-                    <input id="date-picker-input-three" type="text" wire:model="period_from" name="period_from"
+                    <input id="date-picker-input-three" type="text" value="{{ request('s_date') }}" name="s_date"
                         class="form-control mb-3 mb-lg-0" placeholder="{{ __('Date') }}" />
                 </div>
 
@@ -343,7 +340,7 @@
                         <i class="ki-duotone ki-calendar fs-2"><span class="path1"></span><span
                                 class="path2"></span></i>
                     </span>
-                    <input id="date-picker-input-four" type="text" wire:model="period_to" name="period_to"
+                    <input id="date-picker-input-four" type="text" value="{{ request('e_date') }}" name="e_date"
                         class="form-control mb-3 mb-lg-0" placeholder="{{ __('Date') }}" />
                 </div>
 
@@ -392,13 +389,13 @@
                     <span class="indicator-label" wire:loading.remove>{{ __('Soumettre') }}</span>
                 </button>
 
-                <button href="/geolocation/taxpayers" type="submit" class="btn badge-light mt-8">
-                    <span class="indicator-label" wire:loading.remove>{{ __('Rénitialiser') }}</span>
-                </button>
+                <a href="{{ request()->url() }}" class="btn badge-light mt-8">
+                    <span class="indicator-label" >{{ __('Rénitialiser') }}</span>
+                </a>
             </div>
 
         </div>
-
+        </form>
     </div>
 
 
