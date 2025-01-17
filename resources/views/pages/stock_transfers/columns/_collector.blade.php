60d31873a9fc0eaca7aebaef69eb02a16e8bbb08
LanzaDelRelampago <novenosexta77@gmail.com>
2025-01-17 12:08:21 +0000

    fix: display perdiod

--- resources/views/pages/stock_transfers/columns/_collector.blade.php
+++ resources/views/pages/stock_transfers/columns/_collector.blade.php
@@ -14,23 +14,12 @@ $url = "stock-transfers/{$stock_transfer->to_user_id}?p_s_date={$stock_transfer-
 
 @endphp
 <div class="d-flex flex-column">
-    
+
     <a href="{{ $url }}"
        class="{{ $stock_transfer->period_from != $stock_transfer->period_to && $stock_transfer->period_to >= now()->toDateString()
                 ? 'menu-link text-start text-wrap badge badge-warning'
                 : 'menu-link text-start text-wrap' }}">
         {{ $stock_transfer->period_from . " - " . $stock_transfer->period_to }}
     </a>
-
-
-
-@if ($stock_transfer->period_from != $stock_transfer->period_to && $stock_transfer->period_to >= now()->toDateString())
-        <a href="#" class="btn btn-icon btn-light pulse pulse-warning">
-            <i class="ki-duotone ki-element-11 fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i>
-            <span class="pulse-ring"></span>
-        </a>
-        @endif
-
-
 </div>
 <!--begin::User details-->
