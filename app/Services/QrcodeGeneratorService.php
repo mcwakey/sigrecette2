c44db078bb1864505ea26b5a40678b595042d58d
LanzaDelRelampago <novenosexta77@gmail.com>
2025-01-31 11:25:19 +0000

    fix:add qrcode generator in service

--- app/Services/QrcodeGeneratorService.php
+++ app/Services/QrcodeGeneratorService.php
@@ -10,6 +10,7 @@ use chillerlan\QRCode\Output\QRCodeOutputException;
 use chillerlan\QRCode\Output\QRGdImageWEBP;
 use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\QROptions;
+use Exception;
 
 class QrcodeGeneratorService
 {
@@ -22,65 +23,70 @@ class QrcodeGeneratorService
      */
     public function generate(?string $data, ?string $backgroundImagePath = null): mixed
     {
-        if ($data === null) {return null;}
+        try {
+            if ($data === null) {return null;}
 
 
 
-        if ($backgroundImagePath !== null && file_exists($backgroundImagePath)) {
-            //$backgroundImage = imagecreatefromstring(file_get_contents($backgroundImagePath));
+            if ($backgroundImagePath !== null && file_exists($backgroundImagePath)) {
+                //$backgroundImage = imagecreatefromstring(file_get_contents($backgroundImagePath));
 
-            $options = new QROptions;
+                $options = new QROptions;
 
-            $options->version             = 5;
-            $options->outputBase64        = false;
-            $options->scale               = 6;
-            $options->imageTransparent    = false;
-            $options->drawCircularModules = true;
-            $options->circleRadius        = 0.45;
-            $options->keepAsSquare        = [
-                QRMatrix::M_FINDER,
-                QRMatrix::M_FINDER_DOT,
-            ];
-            $options->eccLevel            = EccLevel::H;
-            $options->addLogoSpace        = true;
-            $options->logoSpaceWidth      = 13;
-            $options->logoSpaceHeight     = 13;
+                $options->version             = 5;
+                $options->outputBase64        = false;
+                $options->scale               = 6;
+                $options->imageTransparent    = false;
+                $options->drawCircularModules = true;
+                $options->circleRadius        = 0.45;
+                $options->keepAsSquare        = [
+                    QRMatrix::M_FINDER,
+                    QRMatrix::M_FINDER_DOT,
+                ];
+                $options->eccLevel            = EccLevel::H;
+                $options->addLogoSpace        = true;
+                $options->logoSpaceWidth      = 13;
+                $options->logoSpaceHeight     = 13;
 
-            $qrcode = new QRCode($options);
-            $qrcode->addByteSegment($data);
+                $qrcode = new QRCode($options);
+                $qrcode->addByteSegment($data);
 
-            $qrOutputInterface = new QRImageWithLogo($options, $qrcode->getQRMatrix());
-          //dd($qrOutputInterface->dump(null,$backgroundImagePath));
-            return $qrOutputInterface->dump(null,$backgroundImagePath);
-        }else{
-            $options = new QROptions;
+                $qrOutputInterface = new QRImageWithLogo($options, $qrcode->getQRMatrix());
+                //dd($qrOutputInterface->dump(null,$backgroundImagePath));
+                return $qrOutputInterface->dump(null,$backgroundImagePath);
+            }else{
+                $options = new QROptions;
 
-            $options->outputInterface     = QRGdImageWEBP::class;
-            $options->quality             = 90;
-            $options->scale               = 20;
-            $options->bgColor             = [200, 150, 200];
-            $options->imageTransparent    = true;
-            $options->transparencyColor   = [200, 150, 200];
-            $options->drawCircularModules = true;
-            $options->drawLightModules    = true;
-            $options->circleRadius        = 0.4;
-            $options->keepAsSquare        = [
-                QRMatrix::M_FINDER_DARK,
-                QRMatrix::M_FINDER_DOT,
-                QRMatrix::M_ALIGNMENT_DARK,
-            ];
-            $options->moduleValues        = [
-                QRMatrix::M_FINDER_DARK    => [0, 63, 255], // dark (true)
-                QRMatrix::M_FINDER_DOT     => [0, 63, 255], // finder dot, dark (true)
-                QRMatrix::M_FINDER         => [233, 233, 233], // light (false)
-                QRMatrix::M_ALIGNMENT_DARK => [255, 0, 255],
-                QRMatrix::M_ALIGNMENT      => [233, 233, 233],
-                QRMatrix::M_DATA_DARK      => [0, 0, 0],
-                QRMatrix::M_DATA           => [233, 233, 233],
-            ];
-            return  (new QRCode($options))->render(data: $data);
+                $options->outputInterface     = QRGdImageWEBP::class;
+                $options->quality             = 90;
+                $options->scale               = 20;
+                $options->bgColor             = [200, 150, 200];
+                $options->imageTransparent    = true;
+                $options->transparencyColor   = [200, 150, 200];
+                $options->drawCircularModules = true;
+                $options->drawLightModules    = true;
+                $options->circleRadius        = 0.4;
+                $options->keepAsSquare        = [
+                    QRMatrix::M_FINDER_DARK,
+                    QRMatrix::M_FINDER_DOT,
+                    QRMatrix::M_ALIGNMENT_DARK,
+                ];
+                $options->moduleValues        = [
+                    QRMatrix::M_FINDER_DARK    => [0, 63, 255], // dark (true)
+                    QRMatrix::M_FINDER_DOT     => [0, 63, 255], // finder dot, dark (true)
+                    QRMatrix::M_FINDER         => [233, 233, 233], // light (false)
+                    QRMatrix::M_ALIGNMENT_DARK => [255, 0, 255],
+                    QRMatrix::M_ALIGNMENT      => [233, 233, 233],
+                    QRMatrix::M_DATA_DARK      => [0, 0, 0],
+                    QRMatrix::M_DATA           => [233, 233, 233],
+                ];
+                return  (new QRCode($options))->render(data: $data);
 
+            }
+        }catch (Exception $exception){
+            return null;
         }
+
 
 
     }
