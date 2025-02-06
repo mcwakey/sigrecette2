bdf2c205964a9fa7d49100626ae01d61730e4f47
LanzaDelRelampago <novenosexta77@gmail.com>
2025-02-06 13:46:04 +0000

    feat:optimize status request

--- app/Models/Taxpayer.php
+++ app/Models/Taxpayer.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Models;
+use App\Enums\InvoicePayStatusEnums;
+use App\Enums\InvoiceStatusEnums;
 use App\Enums\TaxpayerStaticsEnums;
 use App\Helpers\Constants;
 use Illuminate\Database\Eloquent\Builder as QueryBuilder;
@@ -152,17 +154,19 @@ class Taxpayer extends Model
     }
     public function getStatus(): bool
     {
-        $status =false;
-        foreach ( $this->invoices as $invoice) {
-            if ($status==false&&$invoice->to_date>now()&&$invoice->isValid()) {
-                $status =true;
-
-                return $status;
-            }
-        }
-        return $status;
-
+        return Invoice::where('taxpayer_id','=',$this->id)
+            ->where('to_date', '>', now())
+            ->whereNotIn('status', [
+                InvoiceStatusEnums::REJECTED_BY_OR,
+                InvoiceStatusEnums::REJECTED,
+                InvoiceStatusEnums::CANCELED,
+                InvoiceStatusEnums::REDUCED
+            ])
+            ->where('pay_status', '!=', InvoicePayStatusEnums::PAID)
+            ->where('validity', 'VALID')
+            ->exists();
     }
+
     /**
      * Search for a given value in multiple columns.
      */
